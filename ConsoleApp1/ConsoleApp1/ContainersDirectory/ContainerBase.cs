7fc63352bf869f0f5400e882ba173cb86feebe79
s27324 <s27324@pjwstk.edu.pl>
2024-03-18 16:48:37 +0100

    Abstract container

---
+++ ConsoleApp1/ConsoleApp1/ContainersDirectory/ContainerBase.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1.ContainersDirectory;
+
+public class ContainerBase
+{
+    protected double CargoWeight;
+    protected double Height;
+    protected double SoleWeight;
+    protected double Depth;
+    private static int number = 0;
+    protected string SerialNumber;
+    protected double MaxPayload;
+
+    public ContainerBase(double cargoWeight, double height, double soleWeight, double depth, string serialNumber, double maxPayload)
+    {
+        CargoWeight = cargoWeight;
+        Height = height;
+        SoleWeight = soleWeight;
+        Depth = depth;
+        SerialNumber = serialNumber;
+        MaxPayload = maxPayload;
+    }
+}
