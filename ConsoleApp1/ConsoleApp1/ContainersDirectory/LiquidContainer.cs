6255cd62b657e7dafd74035784d7b2f1ac345a43
s27324 <s27324@pjwstk.edu.pl>
2024-03-23 00:58:58 +0100

    Replacing NLC to LiquidContainer

--- ConsoleApp1/ConsoleApp1/ContainersDirectory/LiquidContainer.cs
+++ ConsoleApp1/ConsoleApp1/ContainersDirectory/LiquidContainer.cs
@@ -1,8 +1,8 @@
 ﻿namespace ConsoleApp1.ContainersDirectory.LiquidContainers;
 
-public class NormalLiquidContainer : ContainerBase
+public class LiquidContainer : ContainerBase
 {
-    public NormalLiquidContainer(double cargoWeight, double height, double soleWeight, double depth, string serialNumber, double maxPayload) : base(cargoWeight, height, soleWeight, depth, serialNumber, maxPayload)
+    public LiquidContainer(double cargoWeight, double height, double soleWeight, double depth, string serialNumber, double maxPayload) : base(cargoWeight, height, soleWeight, depth, serialNumber, maxPayload)
     {}
 
     public override void ReloadingContainers(double Cargo)
